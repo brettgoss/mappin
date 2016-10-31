6100fd5e8d0ff21d5c855586ab4860e692f6472b
Brett Goss <brettgoss@live.com>
2016-10-31 16:03:25 -0700

    minor syntax corrections

--- db/migrations/20161030001024_new_maps_table.js
+++ db/migrations/20161030001024_new_maps_table.js
@@ -1,10 +1,10 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('maps', function (table) {
-      table.increments();
-      table.integer('user_id').references('id').inTable('users').notNullable();
-      table.text('mapname').notNullable();
-      table.json('fc_mapstate').notNullable();
-    })
+  return knex.schema.createTable('maps', function (table) {
+    table.increments();
+    table.integer('user_id').references('id').inTable('users').notNullable();
+    table.text('mapname').notNullable();
+    table.json('fc_mapstate').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
