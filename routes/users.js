cda477e501424677db8fab5d0d5b12e710aecf43
Brett Goss <brettgoss@live.com>
2016-10-31 03:37:15 -0700

    salted and hashed passwords

--- routes/users.js
+++ routes/users.js
@@ -2,28 +2,51 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt  = require('bcryptjs');
+const salt    = bcrypt.genSaltSync(10);
 
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
     knex
-      .select("username")
+      .select("*")
       .from("users")
       .then((results) => {
         res.json(results);
     });
   })
   router.post("/register", (req, res) => {
-    knex('users')
-      .insert(req.body)
-      .then(() => {
-        console.log(req.body)
-        res.redirect('/')
-      })
+    var user = req.body.username;
+    var pass = req.body.password;
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(pass, salt, function(err, hash) {
+        knex('users')
+          .insert({
+            username: user,
+            password: hash
+          })
+        .then(() => {
+          knex
+            .select('*')
+            .from("users")
+            .where({
+              username: user
+            })
+          .then(function (result){
+            var uid = result[0].id;
+              console.log("Yep")
+              res.cookie('userid', uid);
+              res.cookie('username', user);
+              res.redirect("/");
+          })
+        })
+      });
+    });
   });
   router.post("/login", (req, res) => {
     var user = req.body.username;
     var pass = req.body.password;
+
     knex
       .select('*')
       .from("users")
@@ -31,23 +54,25 @@ module.exports = (knex) => {
         username: user
       })
       .then(function (result){
-          if(result[0].username == user && result[0].password == pass) {
+        bcrypt.compare(pass, result[0].password, function(err, results) {
+          if(result[0].username == user && results) {
             var uid = result[0].id;
             console.log("Yep")
             res.cookie('userid', uid);
             res.cookie('username', user);
             res.redirect("/");
           } else {
-            console.log("Fail")
+            console.log("Invalid Credentials")
             res.redirect("/");
           }
+        })
 
       })
       .catch(function (err){
         console.log("error", err);
         res.redirect("/");
       })
-    })
+  })
 
   router.post("/logout", (req, res) => {
     delete res.clearCookie("username")
